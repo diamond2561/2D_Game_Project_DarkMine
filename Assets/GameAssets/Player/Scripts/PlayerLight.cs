e72a88b03e2cc7c204f3731a7512678eb261b888
diamond2561 <diamond25361082@gmail.com>
2025-05-01 13:31:48 +0300

    PlayerLight

---
+++ Assets/GameAssets/Player/Scripts/PlayerLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerLight : MonoBehaviour
+{
+    [SerializeField] private Light2D _playerLight; // Ссылка на свет игрока 
+
+    public void SetLightDirection(Vector2 direction)
+    {
+        // Вычисляем угол поворота на основе направления
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Применяем угол к объекту (свет всегда направлен вдоль оси X по умолчанию)
+        transform.eulerAngles = new Vector3(0, 0, angle + 90); // -90 для корректировки направления
+    }
+
+    // Включение света игрока
+    public void TurnOffThePlayerLight()
+    {
+        _playerLight.enabled = false;
+    }
+
+    // Выключение света игрока
+    public void TurnOnThePlayerLight()
+    {
+        _playerLight.enabled = true;
+    }
+}
