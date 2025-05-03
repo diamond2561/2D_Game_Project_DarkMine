43efd3fe5dd574d2d774c4b743697f0f575a6b88
diamond2561 <diamond25361082@gmail.com>
2025-05-03 16:42:13 +0300

    PickableItems

---
+++ Assets/GameAssets/PickableItems/Scripts/BasePickableItem/Notes.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+public class Note
+{
+    public string title; // Название заметки
+    public string content; // Текст заметки
+}
---
+++ Assets/GameAssets/PickableItems/Scripts/NewspaperArticles/NewspaperArticle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NewspaperArticle : BasePickableItem
+{
+    public Note note; // Данные заметки
+
+    protected override void OnCollect()
+    {
+        // Добавляем заметку в менеджер заметок
+
+        // NoteManager.Instance.AddNote(note);
+        Debug.Log($"Заметка добавлена: {note.title}\nТекст заметки: {note.content}");
+    }
+}
