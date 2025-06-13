798249889c82be40a88d28fd54bc540daeda6b03
diamond2561 <diamond25361082@gmail.com>
2025-06-13 13:23:04 +0300

    NotePad

--- Assets/GameAssets/Common/LanguageSwither/LanguageData.cs
+++ Assets/GameAssets/Common/LanguageSwither/LanguageData.cs
@@ -113,6 +113,13 @@ public static class LanguageData
                 {"Русский", "Блокнот" }
             }
             },
+            { "pause_text", new Dictionary<string, string>()
+            {
+                {"English", "Pause" },
+                {"Русский", "Пауза" }
+            }
+            },
+
 
 
 
@@ -127,6 +134,32 @@ public static class LanguageData
             {
                 {"English", "Dear... or rather, an unhappy friend. If you're reading this, I'm gone. Like you, I once believed that I could get out. Like you, I was wrong.\r\n\r\nThis is a trap dungeon. And its inhabitants are not just creatures. They are death itself. It's pointless to fight. They can't be killed. You can only hide... until it's too late.\r\n\r\nBut maybe my miserable fate will help you hold out a little longer. There are others here... amorphous, shapeless. They can't see. But they hear perfectly. A step, a sigh, even a gnash of teeth in fear — and they will attack.\r\n\r\nSo remember: if you notice them, freeze. Move, and... however, you'll figure it out for yourself.\r\n\r\nGoodbye. Or see you soon... in oblivion." },
                 {"Русский", "Дорогой... или скорее несчастный друг. Если ты читаешь эти строки — меня уже нет. Как и ты, я когда-то верил, что смогу выбраться. Как и ты, я ошибался.\r\n\r\nЭто подземелье — ловушка. И его обитатели — не просто твари. Они — сама смерть. Бессмысленно сражаться. Их нельзя убить. Можно только прятаться... пока не станет слишком поздно.\r\n\r\nНо, возможно, моя жалкая участь поможет тебе продержаться чуть дольше. Здесь есть другие... аморфные, бесформенные. Они не видят. Но слышат идеально. Шаг, вздох, даже скрип зубов от страха — и они нападут.\r\n\r\nТак что запомни: заметил их — замри. Шевельнешься — и... впрочем, ты сам всё поймешь.\r\n\r\nПрощай. Или до скорой встречи... в небытии." }
+            }
+            },
+
+            { "note_2_title", new Dictionary<string, string>()
+            {
+                {"English", "Note 2 Title" },
+                {"Русский", "Заметка 2" }
+            }
+            },
+            { "note_2_content", new Dictionary<string, string>()
+            {
+                {"English", " Text note 2" },
+                {"Русский", "Текст второй заметки" },
+            }
+            },
+
+            { "note_3_title", new Dictionary<string, string>()
+            {
+                {"English", "Note 3 Title" },
+                {"Русский", "Заметка 3" }
+            }
+            },
+            { "note_3_content", new Dictionary<string, string>()
+            {
+                {"English", " Text note 3" },
+                {"Русский", "Текст 3 заметки" },
             }
             },
         };
---
+++ Assets/GameAssets/Common/Scripts/GamePause.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Игра на паузе
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f; // Игра продолжается
+    }
+}
