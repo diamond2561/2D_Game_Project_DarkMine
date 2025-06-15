eec3ed2de6c77b2984d56cf34f96d18c56537f60
diamond2561 <diamond25361082@gmail.com>
2025-06-15 15:11:10 +0300

    ResolutionFixes

---
+++ Assets/GameAssets/PickableItems/Scripts/BasePickableItem/NoteData.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class NoteData
+{
+    public string titleKey;
+    public string contentKey;
+
+    public NoteData(string titleKey, string contentKey)
+    {
+        this.titleKey = titleKey;
+        this.contentKey = contentKey;
+    }
+}
