ff8a0100d81da6a3181a9d0f74228d718860a297
diamond2561 <diamond25361082@gmail.com>
2025-06-17 15:54:15 +0300

    SoundTrigger

---
+++ Assets/GameAssets/TriggeredZones/SoundTriggeredZone/Scripts/SoundTriggersZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTriggersZone : MonoBehaviour
+{
+    // Список AudioSource, из которых будет выбираться случайный звук
+    [SerializeField] private List<AudioSource> _triggerSounds = new List<AudioSource>();
+
+    public void PlayRandomTriggerSound()
+    {
+        // Проверяем, есть ли звуки в списке
+        if (_triggerSounds == null || _triggerSounds.Count == 0)
+        {
+            Debug.LogWarning("Список звуков пуст!");
+            return;
+        }
+
+        // Выбираем случайный AudioSource из списка
+        int randomIndex = Random.Range(0, _triggerSounds.Count);
+        AudioSource randomSound = _triggerSounds[randomIndex];
+
+        // Воспроизводим выбранный звук
+        randomSound.Play();
+        Debug.Log($"Played sound: {randomSound.clip.name}");
+    }
+}
